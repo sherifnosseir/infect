08ca8b3b6ec1786024da900b66ac6c182dd3a2a9
Greg Zhou <bottlezz2002@gmail.com>
2013-05-04 20:22:37 -0700

    add test.nmidi

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+var app = require('http').createServer(handler), io = require('socket.io').listen(app), fs = require('fs')
 
 app.listen(80);
 
-function handler (req, res) {
-  fs.readFile(__dirname + req.rul,
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+function handler(req, res) {
+	fs.readFile(__dirname + req.url, function(err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
 
-    res.writeHead(200);
-    res.end(data);
-  });
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
-
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-  socket.on('play',function(){
-  	socket.broadcast.emit('playnote',90);
-  })
+var midifile;
+fs.readFile('/test.mid', function (err, data) {
+  if (err) throw err;
+  midifile=data;
 });
+
+io.sockets.on('connection', function(socket) {
+	socket.emit('news', {
+		hello : 'world'
+	});
+	socket.on('my other event', function(data) {
+		console.log(data);
+	});
+	function process(file) {
+		
+		console.log(file);
+	}
+
+
+	socket.on('play', function() {
+		socket.broadcast.emit('playnote', 90);
+	})
+	process(midifile);
+
+}); 
